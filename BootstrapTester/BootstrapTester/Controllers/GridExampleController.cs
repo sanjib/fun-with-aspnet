6a69e31411b739617db1f7ebc290c5f0f51b543b
Sanjib Ahmad <sanjib@sanjib.org>
2020-09-25 00:56:23 +0800

    Create BootstrapTester project, add examples for grid and forms

---
+++ BootstrapTester/BootstrapTester/Controllers/GridExampleController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BootstrapTester.Controllers
+{
+    public class GridExampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
